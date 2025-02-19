f21fa9e5feb3df7864d15e723c09ceef37cd3c30
Wesley Quintero <johnwesleyquintero@gmail.com>
2025-02-19 17:35:51 -0500

    debug

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "startMonitoring" && !monitoringActive) {
     monitoringActive = true;
     // Instead of injecting the script, send a message to the existing content script
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true, url: "https://sellercentral.amazon.com/*" }, function(tabs) {
       if (tabs[0]) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "startMonitoring" });
       }
@@ -28,7 +28,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.query({ url: "https://sellercentral.amazon.com/*" }, (tabs) => {
     tabs.forEach(tab => {
       chrome.scripting.executeScript({
-        target: { tabId: tab.id },
+        target: { tabId: tab.id, allFrames: true },
         files: ["content.js"]
       }).catch(err => console.error('Failed to inject content script', err));
     });
